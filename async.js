fc4e162a7cd672001f980a51b07a664ef87b7de1
Vishesh <visheshsingh16@gmail.com>
2017-09-21 00:32:30 -0400

    jquery method of getting AJAX requests

--- async.js
+++ async.js
@@ -5,14 +5,23 @@ window.onload = function() {
     http.onreadystatechange = function(){
         //console.log(http);
        if(http.readyState == 4 && http.status == 200){
-            console.log(JSON.parse(http.response));
+            //console.log(JSON.parse(http.response));
         }
     };
     
     //Sets up request for us; tells JS where to get the request from; and whether it is async or not
-    http.open("GET", "data/tweets.json", false);
+    http.open("GET", "data/tweets.json", true);
     http.send();
-    console.log('test');
+    //console.log('test');
+
+    //jQuery method of AJAX requests
+
+    //This takes in two parameters - 1. the data(json) being retrieved; 2. CALLBACK function
+    $.get("data/tweets.json", function(data){
+        console.log(data);
+    });
+    
+    console.log("test");
 }
 
 /*
