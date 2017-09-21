95a535599aa823cb47cb6fbd60c2be6ddb253a21
Vishesh <visheshsingh16@gmail.com>
2017-09-21 00:40:50 -0400

    Callback function - EXAMPLE #2

--- async.js
+++ async.js
@@ -2,9 +2,11 @@ window.onload = function() {
 
     //CALLBACK is just a function which is provided as a parameter to other methods such as
     //forEach() and it is invoked at a certain point of time
-    var animals = ["dog","cat","bunny"];
-    animals.forEach(function(data){
+    function callback(data){
         console.log(data);
-    })
+    }
+
+    var animals = ["dog","cat","bunny"];
+    animals.forEach(callback);
 }
 
